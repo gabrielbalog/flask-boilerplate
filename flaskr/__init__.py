cfd6b4cc00cf24c1682b7ed09060c8c191233307
Gabriel <gabrielbalog68@gmail.com>
2020-02-28 13:55:36 -0300

    Adding suport to flask-sqlalchemy and converting models into classes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,15 +5,22 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
+
     app = Flask(__name__, instance_relative_config=True)
+
+    database_uri = "sqlite:///{}".format(
+        os.path.join(app.instance_path, "flaskr.sqlite")
+    )
+
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI=database_uri,
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -25,14 +32,16 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World!'
+        return "Hello, World!"
 
     from . import db
+
     db.init_app(app)
 
-    from . import auth
+    from flaskr import auth
+
     app.register_blueprint(auth.bp)
 
     return app
